2f8258ef394f1ed4bcc9f2ad84fc6d4fc9ddf769
Rishikesh <kendairishikesh5@gmail.com>
2025-05-21 19:41:01 +0530

    refactor: update old route to new format

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,25 +5,34 @@ import App from './App.tsx'
 import { Toaster } from 'sonner'
 import Profile from './pages/Profile.tsx'
 import Notfound from './pages/Notfound.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 
 const queryClient = new QueryClient()
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/profile/:github_user",
+    element: <Profile />,
+  },
+  {
+    path: "*",
+    element: <Notfound />,
+  },
+])
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<App />} />
-          <Route path='/profile/:github_user' element={<Profile />} />
-          <Route path='*' element={<Notfound />} />
-        </Routes>
-        <div className="layer-wrapper">
+      <RouterProvider router={router} />
+      <div className="layer-wrapper">
         <div className="layer-blur"></div>
-        </div>
-        <Toaster position='bottom-left' />
-      </BrowserRouter>
+      </div>
+      <Toaster position='bottom-left' />
     </QueryClientProvider>
   </StrictMode>,
 )
